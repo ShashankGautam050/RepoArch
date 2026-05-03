df872f82bc2062b57eac4c0af89e282615a219a6
Shashank Gautam <shashank320722@gmail.com>
2026-05-03 10:17:03 +0530

    Initial Commit

---
+++ RepoArch/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  RepoArch
+//
+//  Created by Shashank Gautam on 03/05/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ RepoArch/RepoArchApp.swift
@@ -0,0 +1,17 @@
+//
+//  RepoArchApp.swift
+//  RepoArch
+//
+//  Created by Shashank Gautam on 03/05/26.
+//
+
+import SwiftUI
+
+@main
+struct RepoArchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
